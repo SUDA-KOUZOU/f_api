de2db7d42ea374d9979a20e22e7829dffd8a254e
SUDA-KOUZOU <yamanasi1934@gmail.com>
2022-05-03 12:03:13 +0900

    うまくいくか？

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from typing import List
+from models import User, Gender, Role
+from uuid import uuid4
 
 app = FastAPI()
 
+db: List[User] = [
+    User(
+        id=uuid4(), 
+        first_name="Jamila",
+        last_name="Ahmed",
+        gender=Gender.female,
+        roles=[Role.student]
+    ),
+    User(
+        id=uuid4(),
+        first_name="Alex",
+        last_name="Jones",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
+    )
+]
 @app.get("/")
-def root():
+async def root():
     return {"Hello": "Mundo"}
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;
